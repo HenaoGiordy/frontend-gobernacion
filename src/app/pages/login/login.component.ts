db657234bbe0a066bdfd91df57b88adb5ada7718
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-01-30 23:43:25 -0500

    vista login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, CardModule,InputTextModule,PasswordModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
