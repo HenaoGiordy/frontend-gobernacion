886431e485507462c48bef6a43da4330ca90b2e6
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-03-13 20:41:49 -0500

    agregar registro

--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -3,14 +3,14 @@ import { TableModule } from 'primeng/table';
 import { DataTable } from '../../interfaces/dataTable';
 
 import { ButtonModule } from 'primeng/button';
-
+import { DialogoComponent } from '../dialogo/dialogo.component';
 
 
 
 @Component({
   selector: 'app-tabla',
   standalone: true,
-  imports: [TableModule, ButtonModule],
+  imports: [TableModule, ButtonModule, DialogoComponent],
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css',
 })
