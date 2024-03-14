5d74bbfcd74db02556ab12ed4b00e0b2347868b3
Giordy Henao <Henaogiordy@gmail.com>
2024-03-13 20:45:22 -0500

    .

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,10 +3,13 @@ import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { ListboxModule } from 'primeng/listbox';
 
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, ListboxModule],
+  imports: [SidebarModule, ButtonModule, ListboxModule, AvatarModule, AvatarGroupModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
